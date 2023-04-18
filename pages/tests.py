a709b119b0a2432e2c78a885d4919bed7f67383d
fersus <deriabin_85@mail.ru>
2023-04-18 10:58:42 +0300

    add templates, homepage and tests for it

---
+++ pages/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'i should not be on he page')
